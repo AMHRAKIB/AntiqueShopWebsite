d47a4afd833650c65e794a711119f470c40e2079
Wordh Ul Hasan <wordhulhasan@yahoo.com>
2018-12-04 11:43:08 -0800

    Add files via upload

---
+++ products/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+
+def home(request):
+    if request.user.is_authenticated:
+        username_is="Random"
+        context={"username_is":request.user}
+    else:
+        context = {"username_is": request.user}
+    template = 'products/home.html'
+    return render(request, template, context)
